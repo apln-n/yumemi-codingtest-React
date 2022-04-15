a9c18409a547362f5762494ad72806de55821808
apln-n <applingo@eis.hokudai.ac.jp>
2022-04-15 14:58:34 +0900

    RESAS APIから都道府県一覧を取得(テキストベース)

--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const apiKey = "p9t4mVKpr6vhGAwius3ldgTVMk9YNa1nARFDSLmr";
+
+const getPrefsDict = () => {
+  const path = "https://opendata.resas-portal.go.jp/api/v1/prefectures";
+  const request = new XMLHttpRequest();
+  request.open('GET',path,false)
+  request.setRequestHeader("X-API-KEY", apiKey);
+  request.send(null);
+  const prefs = request.response;
+  //const prefs = JSON.parse(request.response);
+  return prefs;
+}
 
 const Title = () => {
   return <h1>Title</h1>;
@@ -12,7 +24,12 @@ const CheckBoxes = () => {
 };
 
 const Graph = () => {
-  return <h2>Graph</h2>;
+  return (
+    <div>
+      <h2>Graph</h2>
+      <div>{getPrefsDict()}</div>
+    </div>
+  );
 };
 
 const App = () => {
