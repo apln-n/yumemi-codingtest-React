9009f329a922a8797ce83fdadcd2c6b01456742d
apln-n <applingo@eis.hokudai.ac.jp>
2022-04-15 15:13:27 +0900

    都道府県の名前の一覧をリストで返す関数の作成

--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -4,15 +4,23 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const apiKey = "p9t4mVKpr6vhGAwius3ldgTVMk9YNa1nARFDSLmr";
 
-const getPrefsDict = () => {
+//都道府県一覧のリスト
+const getPrefsList = () => {
   const path = "https://opendata.resas-portal.go.jp/api/v1/prefectures";
   const request = new XMLHttpRequest();
   request.open('GET',path,false)
   request.setRequestHeader("X-API-KEY", apiKey);
   request.send(null);
-  const prefs = request.response;
-  //const prefs = JSON.parse(request.response);
-  return prefs;
+  const prefs = JSON.parse(request.response);
+  /*
+    JSONのフォーマット:
+    {"message":null,"result":[{"prefCode":1,"prefName":"北海道"},{"prefCode":2...}...]}
+  */
+  let prefsList = [];
+  for(let i=0;i<prefs.result.length;i++){
+    prefsList.push(prefs.result[i].prefName);
+  }
+  return prefsList;
 }
 
 const Title = () => {
@@ -20,14 +28,19 @@ const Title = () => {
 };
 
 const CheckBoxes = () => {
-  return <h2>CheckBoxes</h2>;
+  return (
+    <div>
+      <h2>CheckBoxes</h2>
+      <div>{getPrefsList()}</div>
+    </div>
+  );
 };
 
 const Graph = () => {
   return (
     <div>
       <h2>Graph</h2>
-      <div>{getPrefsDict()}</div>
+      <div></div>
     </div>
   );
 };
