5c08b6e7d2d79cf40073f7c72b32ca644ce28609
apln-n <applingo@eis.hokudai.ac.jp>
2022-04-15 11:38:39 +0900

    ワイヤーフレームの大枠だけ作成

--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -3,11 +3,32 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const Title = () => {
+  return(
+    <h1>Title</h1>
+  );
+}
+
+const CheckBoxes = () => {
+  return(
+    <h2>CheckBoxes</h2>
+  );
+}
+
+const Graph = () => {
+  return (
+    <h2>Graph</h2>
+  );
+}
 
 const App = () => {
-  const myname = "OsakaNaoki";
-  const element = <h1>Hello, {myname}.</h1>
-  return element;
+  return (
+    <div>
+      <Title />
+      <CheckBoxes />
+      <Graph />
+    </div>
+  );
 }
 
 root.render(
